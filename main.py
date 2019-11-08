79a6b326a6cfd6e562e238f2730049854df5c748
Khaled Soliman <khaledsoliman@trukto.com>
2019-11-08 19:34:21 +0200

    Parsed LEF|DEF|LIB

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello')
+from liberty.parser import parse_liberty
+from lef_parser import LefParser
+from def_parser import DefParser
+liberty_file = "Testing Files/osu035.lib"
+lef_file = "Testing Files/osu035_stdcells.lef"
+def_file = "Testing Files/timer.def"
+library = parse_liberty(open(liberty_file).read())
+lefFile = LefParser(lef_file)
+defFile = DefParser(def_file)
+print(str(library))
